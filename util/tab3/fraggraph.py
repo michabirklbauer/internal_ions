afb7efab1395f45c8c0a2930ce5ea74419852ef2
Lev Levitsky <lev.levitsky@phystech.su>
2025-02-25 18:57:00 +0100

    Reduce code duplication in util/tab3/fraggraph

--- util/tab3/fraggraph.py
+++ util/tab3/fraggraph.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 import shutil
 import random
@@ -22,7 +20,7 @@ from util.tab3.plots import draw_barplot_intensity_SDI
 from util.constants import DIV_COLOR
 
 
-def single_fraggraph(pep1: str, verbose: bool = False) -> None:
+def create_fraggraph(peptidoforms):
     with st.status("Generating the fragmention graph...") as fg_gen_status:
         with st_stdout("info"):
             start_ioncaps_types = st.session_state["selected_ions_cterm"]
@@ -52,12 +50,17 @@ def single_fraggraph(pep1: str, verbose: bool = False) -> None:
                            max_isotope=max_isotope,
                            charge_loss=charge_loss)
 
-            fg.generate_graph([pep1],
+            fg.generate_graph(peptidoforms,
                               st.session_state["consensus_spectrum"]["mz_mean"],
                               st.session_state["consensus_spectrum"]["its_mean"])
 
         st.success("Successfully generated fragmentation graph!")
-        fg_gen_status.update(label=f"Successfully generated fragmentation graph for {pep1}.", state="complete")
+        fg_gen_status.update(label=f"Successfully generated fragmentation graph for {', '.join(peptidoforms)}.", state="complete")
+    return fg
+
+
+def single_or_double_fraggraph(peptidoforms: list[str], verbose: bool = False) -> None:
+    fg = create_fraggraph(peptidoforms)
 
     # file storage handling
     tmp_dir_name = "tmp_fragannot_files_653205774"
@@ -71,99 +74,33 @@ def single_fraggraph(pep1: str, verbose: bool = False) -> None:
     draw_graph(fg, output_filename=output_name_prefix + "pyvis.html")
     with open(output_name_prefix + "pyvis.html", "r", encoding="utf-8") as f:
         graph = f.read()
-        f.close()
 
     st.subheader("Visualization of the fragmentation graph", divider=DIV_COLOR)
     # st.markdown("Description text")
-
     components.html(graph, width=900, height=900)
 
-    frag_mat_plot = draw_fragment_coverage_matrix_plotly(fg, x="intensity", filename=None)
+    cols = st.columns(len(peptidoforms))
 
-    st.markdown("**Fragment Coverage Matrix:**")
-    st.plotly_chart(frag_mat_plot, use_container_width=False, height=1200, width=800)
+    for i, col in enumerate(cols):
+        with col:
+            st.markdown(f"**Fragment Coverage Matrix Peptidoform {i+1}:**")
+            frag_mat_plot = draw_fragment_coverage_matrix_plotly(fg, x="intensity", peptidoform_index=i)
+            st.plotly_chart(frag_mat_plot, use_container_width=False, height=500, width=500)
+
+    if len(peptidoforms) > 1:
+        diff_plot = draw_fragment_coverage_matrix_difference_plotly(fg)
+        st.markdown("**Fragment Coverage Matrix Difference:**")
+        st.plotly_chart(diff_plot, use_container_width=False, height=600, width=600)
+
+        sdi_plot = draw_barplot_intensity_SDI(fg)
+        st.markdown("**Barplot of Intensity SDI:**")
+        st.plotly_chart(sdi_plot, use_container_width=False, height=600, width=600)
 
     try:
         os.remove(output_name_prefix + "pyvis.html")
     except Exception:
         if verbose:
             print("Could not remove file: " + output_name_prefix + "pyvis.html")
-
-    return
-
-
-def double_fraggraph(pep1: str, pep2: str, verbose: bool = False) -> None:
-    with st.status("Generating the fragmentation graph...") as fg_gen_status:
-        with st_stdout("info"):
-            start_ioncaps_types = st.session_state["selected_ions_cterm"]
-            end_ioncaps_types = st.session_state["selected_ions_nterm"]
-            msms_tol = st.session_state["tolerance"]
-            msms_tol_unit = "da"
-
-            if st.session_state["deconvoluted_spectra"]:
-                monoisotopic = True
-                max_prec_charge = 1
-                charge_loss = False
-            else:
-                monoisotopic = False
-                charge_loss = st.session_state["charge_reduction"]
-                max_prec_charge = st.session_state["max_charge_auto"] if st.session_state["max_charge_auto"] else st.session_state["max_charge"]
-                max_isotope = st.session_state["max_isotope_auto"] if st.session_state["max_isotope_auto"] else st.session_state["max_isotope"]
-
-            print(f"Parameters: {start_ioncaps_types}, {end_ioncaps_types}, {msms_tol}, {msms_tol_unit}, {monoisotopic}, {max_prec_charge}, {max_isotope}, {charge_loss}")
-
-            fg = FragGraph(start_ioncaps_types=start_ioncaps_types,
-                           end_ioncaps_types=end_ioncaps_types,
-                           msms_tol=msms_tol,
-                           msms_tol_unit=msms_tol_unit,
-                           monoisotopic=monoisotopic,
-                           max_prec_charge=max_prec_charge,
-                           max_isotope=max_isotope,
-                           charge_loss=charge_loss)
-
-            fg.generate_graph([pep1, pep2],
-                              st.session_state["consensus_spectrum"]["mz_mean"],
-                              st.session_state["consensus_spectrum"]["its_mean"])
-
-        st.success("Successfully generated fragmentation graph!")
-        fg_gen_status.update(label=f"Successfully generated fragmentation graph for {pep1} and {pep2}.", state="complete")
-
-    # file storage handling
-    tmp_dir_name = "tmp_fragannot_files_653205774"
-    if os.path.exists(tmp_dir_name) and os.path.isdir(tmp_dir_name):
-        shutil.rmtree(tmp_dir_name)
-    os.makedirs(tmp_dir_name)
-
-    output_name_prefix = tmp_dir_name + "/" + datetime.now().strftime("%b-%d-%Y_%H-%M-%S") + "_" + str(random.randint(10000, 99999))
-
-    # visualizing the fragmentation graph
-    draw_graph(fg, output_filename=output_name_prefix + "pyvis.html")
-    with open(output_name_prefix + "pyvis.html", "r", encoding="utf-8") as f:
-        graph = f.read()
-
-    st.subheader("Visualization of the fragmentation graph", divider=DIV_COLOR)
-    st.markdown("Description text")
-    components.html(graph, width=900, height=900)
-
-    plot_col1, plot_col2 = st.columns(2)
-
-    with plot_col1:
-        st.markdown("**Fragment Coverage Matrix Peptidoform 1:**")
-        frag_mat_plot_1 = draw_fragment_coverage_matrix_plotly(fg, x="intensity", peptidoform_index=0)
-        st.plotly_chart(frag_mat_plot_1, use_container_width=False, height=500, width=500)
-
-    with plot_col2:
-        st.markdown("**Fragment Coverage Matrix Peptidoform 2:**")
-        frag_mat_plot_2 = draw_fragment_coverage_matrix_plotly(fg, x="intensity", peptidoform_index=1)
-        st.plotly_chart(frag_mat_plot_2, use_container_width=False, height=500, width=500)
-
-    diff_plot = draw_fragment_coverage_matrix_difference_plotly(fg)
-    st.markdown("**Fragment Coverage Matrix Difference:**")
-    st.plotly_chart(diff_plot, use_container_width=False, height=600, width=600)
-
-    sdi_plot = draw_barplot_intensity_SDI(fg)
-    st.markdown("**Barplot of Intensity SDI:**")
-    st.plotly_chart(sdi_plot, use_container_width=False, height=600, width=600)
 
 
 def main(argv=None) -> None:
@@ -205,8 +142,8 @@ def main(argv=None) -> None:
         if params["pep1"] is None and params["pep2"] is None:
             st.error("Please select or provide at least one peptidoform/proteoform to run Fraggraph!", icon="🚨")
         elif params["pep1"] is None and params["pep2"] is not None:
-            single_fraggraph(params["pep2"])
+            single_or_double_fraggraph([params["pep2"]])
         elif params["pep2"] is None and params["pep1"] is not None:
-            single_fraggraph(params["pep1"])
+            single_or_double_fraggraph([params["pep1"]])
         else:
-            double_fraggraph(params["pep1"], params["pep2"])
+            single_or_double_fraggraph([params["pep1"], params["pep2"]])
